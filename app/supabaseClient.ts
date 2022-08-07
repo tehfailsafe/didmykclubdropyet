b5f09547646b8c98c0c8dc4d8960d874c53f9c48
Mike <mike@failsafedesign.com>
2022-08-06 22:38:38 -0500

    update

--- app/supabaseClient.ts
+++ app/supabaseClient.ts
@@ -4,5 +4,3 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl as string, supabaseAnonKey as string);
-
-//M6Hq@6JLScRDjTy
